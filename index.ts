9defba38e9952553d9b7345a312361e72d3a7cd6
Docenko Pasha <pashadocenko@gmail.com>
2024-03-01 23:47:57 +0300

    connection to db was added. (works)

--- index.ts
+++ index.ts
@@ -11,25 +11,6 @@ const port: number = parseInt(process.env.PORT!, 10) || 5001;
 app.use(cors());
 app.use(express.json());
 
-// app.post("/create_table", async (req: Request, res: Response) => {
-//     try {
-//         // SQL-запрос для создания таблицы (здесь просто пример)
-//         const createTableQuery = `
-//             CREATE TABLE IF NOT EXISTS example_table (
-//                 id INT AUTO_INCREMENT PRIMARY KEY,
-//                 name VARCHAR(255) NOT NULL
-//             )
-//         `;
-//
-//         // Выполнение SQL-запроса к базе данных
-//         await pool.query(createTableQuery);
-//
-//         res.status(200).send("Таблица успешно создана");
-//     } catch (error) {
-//         console.error("Ошибка при создании таблицы:", error);
-//         res.status(500).send("Ошибка при создании таблицы");
-//     }
-// });
 
 const start = async () => {
     try {
