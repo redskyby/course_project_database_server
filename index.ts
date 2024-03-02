b02d30339153637fa6d21ab980a3db72965cc58a
Docenko Pasha <pashadocenko@gmail.com>
2024-03-02 23:43:46 +0300

    Routes was started to init.

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import { config } from "dotenv";
 import cors from "cors";
 import pool from "./db_connection";
+import routes from "./routes";
 
 config();
 
@@ -10,7 +11,7 @@ const port: number = parseInt(process.env.PORT!, 10) || 5001;
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/api/", routes);
 
 const start = async () => {
     try {
