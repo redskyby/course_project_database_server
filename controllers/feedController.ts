94bb63dc2513379a0c41b14a3ca9d34db552c9ee
Docenko Pasha <pashadocenko@gmail.com>
2024-03-10 20:41:38 +0300

    1)Feed of routes were added. 2)feedController.ts was added (addFeed).

---
+++ controllers/feedController.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from "express";
+import pool from "../db_connection";
+class FeedController {
+
+
+    async addFeed(req: Request, res: Response){
+        try {
+            const {name , nameSupplier , typeOfFeed , size , price , date} = req.body
+
+            const sql = `
+                INSERT INTO feed
+                (name, nameSupplier, typeOfFeed, size, price, date)
+                VALUES (?, ?, ?, ?, ?, ?)
+            `;
+
+            const values = [
+                name , nameSupplier , typeOfFeed , size , price , date
+            ]
+
+
+            const result = await pool.query(sql, values);
+
+            return res.status(201).json(result);
+        }catch (e: any) {
+            console.error(e.message); // Вывод ошибки в консоль для дальнейшей диагностики
+            res.status(500).json(e.message);
+        }
+    }
+}
+
+export default new FeedController();
--- routes/index.ts
+++ routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import animalRoutes from "./animalRoutes";
+import feedRoutes from "./feedRoutes";
 const router = Router();
 
 router.use("/animal", animalRoutes);
+router.use("/feed", feedRoutes);
 
 export default router;
