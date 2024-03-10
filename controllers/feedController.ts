d82295cea2ac951096d25a3b10dd0eb4813db39a
Docenko Pasha <pashadocenko@gmail.com>
2024-03-10 20:49:52 +0300

    1)new route was added (getAllFeed) 2)New method was added in feedController.ts

--- controllers/feedController.ts
+++ controllers/feedController.ts
@@ -1,11 +1,9 @@
 import { Request, Response } from "express";
 import pool from "../db_connection";
 class FeedController {
-
-
-    async addFeed(req: Request, res: Response){
+    async addFeed(req: Request, res: Response) {
         try {
-            const {name , nameSupplier , typeOfFeed , size , price , date} = req.body
+            const { name, nameSupplier, typeOfFeed, size, price, date } = req.body;
 
             const sql = `
                 INSERT INTO feed
@@ -13,15 +11,22 @@ class FeedController {
                 VALUES (?, ?, ?, ?, ?, ?)
             `;
 
-            const values = [
-                name , nameSupplier , typeOfFeed , size , price , date
-            ]
-
+            const values = [name, nameSupplier, typeOfFeed, size, price, date];
 
             const result = await pool.query(sql, values);
 
             return res.status(201).json(result);
-        }catch (e: any) {
+        } catch (e: any) {
+            console.error(e.message); // Вывод ошибки в консоль для дальнейшей диагностики
+            res.status(500).json(e.message);
+        }
+    }
+
+    async getAllFeed(req: Request, res: Response) {
+        try {
+            const allFeed = await pool.query("SELECT * FROM feed");
+            res.status(200).json(allFeed[0]);
+        } catch (e: any) {
             console.error(e.message); // Вывод ошибки в консоль для дальнейшей диагностики
             res.status(500).json(e.message);
         }
--- routes/feedRoutes.ts
+++ routes/feedRoutes.ts
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import feedController from "../controllers/feedController";
 
-
 const router = Router();
 
 router.post("/add", feedController.addFeed);
+router.get("/getAllFeed", feedController.getAllFeed);
 
 export default router;
