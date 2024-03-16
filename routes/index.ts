0ce6bac8cacc67d558c9a222b308e789e088d9c3
Docenko Pasha <pashadocenko@gmail.com>
2024-03-16 20:57:09 +0300

    vaccinationRoutes.ts was added.

--- routes/index.ts
+++ routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 import animalRoutes from "./animalRoutes";
 import feedRoutes from "./feedRoutes";
 import positionRoutes from "./positionRoutes";
+import vaccinationRoutes from "./vaccinationRoutes";
 const router = Router();
 
 router.use("/animal", animalRoutes);
 router.use("/feed", feedRoutes);
 router.use("/position", positionRoutes);
+router.use("/vaccination", vaccinationRoutes);
 
 export default router;
