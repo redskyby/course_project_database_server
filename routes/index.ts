2e43076a34777c8956d1bf47fc843b1323385340
Docenko Pasha <pashadocenko@gmail.com>
2024-03-20 13:08:09 +0300

    zoosRoutes.ts were added.

--- routes/index.ts
+++ routes/index.ts
@@ -4,6 +4,7 @@ import feedRoutes from "./feedRoutes";
 import positionRoutes from "./positionRoutes";
 import vaccinationRoutes from "./vaccinationRoutes";
 import illnessesRoutes from "./illnessesRoutes";
+import zoosRoutes from "./zoosRoutes";
 const router = Router();
 
 router.use("/animal", animalRoutes);
@@ -11,5 +12,6 @@ router.use("/feed", feedRoutes);
 router.use("/position", positionRoutes);
 router.use("/vaccination", vaccinationRoutes);
 router.use("/illnesses", illnessesRoutes);
+router.use("/zoo", zoosRoutes);
 
 export default router;
