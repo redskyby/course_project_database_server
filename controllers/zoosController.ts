17c34a766339d09977b41fc09a4d7679e4c4d5c7
Docenko Pasha <pashadocenko@gmail.com>
2024-03-21 13:50:50 +0300

    zoosController.ts works (sortBy)

--- controllers/zoosController.ts
+++ controllers/zoosController.ts
@@ -65,12 +65,12 @@ class ZoosController {
                 return res.status(400).json({ message: "Не указано поле для сортировки" });
             }
 
-            const sql = `SELECT * FROM vaccination ORDER BY ${sort}`;
+            const sql = `SELECT * FROM zoos ORDER BY ${sort}`;
 
-            const sortedVaccinations = await pool.query(sql);
+            const sortedZoos = await pool.query(sql);
 
             // Возвращаем отсортированные данные
-            res.status(200).json(sortedVaccinations[0]);
+            res.status(200).json(sortedZoos[0]);
         } catch (e: any) {
             console.error(e.message); // Вывод ошибки в консоль для дальнейшей диагностики
             res.status(500).json(e.message);
