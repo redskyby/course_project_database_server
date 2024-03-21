663a64b1b3b077de52f5c95a135b53c2c2fd3082
Docenko Pasha <pashadocenko@gmail.com>
2024-03-21 13:17:43 +0300

    zoosRoutes.ts were started to init.

--- routes/zoosRoutes.ts
+++ routes/zoosRoutes.ts
@@ -1,5 +1,11 @@
 import { Router } from "express";
+import vaccinationController from "../controllers/vaccinationController";
 
 const router = Router();
 
+router.post("/add", vaccinationController.addVaccination);
+router.get("/getAllZoos", vaccinationController.getAllVaccinations);
+router.delete("/deleteZoo", vaccinationController.deleteVaccination);
+router.get("/sortBy", vaccinationController.sortBy);
+router.put("/editVaccinationById", vaccinationController.editVaccinationById);
 export default router;
